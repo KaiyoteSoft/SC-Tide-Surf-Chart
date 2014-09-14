2083605d5d90792f30fa59382298bb1708cfeece
KaiyoteSoft <Spikeyhedgehog141@gmail.com>
2014-09-14 10:58:09 -0700

    Broke the code into functions

--- main.py
+++ main.py
@@ -22,9 +22,15 @@ WHITE = ((255, 255, 255))
 ### gets the current day
 
 change_day = 0
+
 redraw = False
 
-def delta(delta_day = 0):
+def get_date(delta_day = 0):
+    """
+    Creates the date string by receiving days it has to forecast
+    :param delta_day:
+    :return: date_string
+    """
     change_time = datetime.datetime.now()
     change_time = change_time + datetime.timedelta(days=delta_day)
     month = str(change_time.month)
@@ -34,19 +40,59 @@ def delta(delta_day = 0):
     if len(day) < 2:
         day = str(0) + day
 
-    change_date = str(change_time.year) + month + day
-    print(change_date)
+    date_string = str(change_time.year) + month + day
+    return(date_string)
+
 
 ## open, read, load the tide
-    info = urllib2.urlopen("http://api.spitcast.com/api/county/tide/santa-cruz/? dval={}".format(change_date))
+def get_tide(date_string):
+    """
+    Recevies date_string and forecasts the tide
+    :param date_string:
+    :return: date, j_data
+    """
+    spit_date = get_date(date_string)
+    info = urllib2.urlopen("http://api.spitcast.com/api/county/tide/santa-cruz/? dval={}".format(spit_date))
     data = info.read()
     j_data = json.loads(data)
     date = (j_data[0]["date"])
 
     return(date, j_data)
 
+def get_graph(j_data):
+    """
+    Gets j_data and constructs the points needed for the graph
+    :param j_data:
+    :return: point_list, time_labels, time_axis
+    """
+    x = 10
+    draw_line = True
+    point_list = []
+    time_labels = []
+    time_axis = []
+    for tide_number in range(1, 25):
+        j_tide = screen_y - int((j_data[tide_number]["tide"]) * 66 + 73)
+        y = j_tide
+        j_time = (j_data[tide_number]["hour"])
+        print(x, y)
+        x = x + 49
+        point_list.append((x, y))
+        if draw_line == True:
+            time_font = font.render("{}".format(j_time), True, WHITE)
+            time_rect = (x, 720, 30, 20)
+            pygame.draw.line(windowSurface, WHITE, (x, 250), (x, 700))
+            draw_line = False
+            time_labels.append(time_font)
+            time_axis.append(time_rect)
+
+        else:
+            draw_line = True
+
+    return(point_list, time_labels, time_axis)
+
 # current_time = time.ctime(date)
-date, j_data = delta()
+date_string = get_date()
+date, j_data = get_tide(change_day)
 
 ## load fonts
 font = pygame.font.Font("fonts/animeace2_reg.ttf", 15)
@@ -79,26 +125,9 @@ point_list = []
 y_axis_start = []
 y_axis_end = []
 
-# def draw_window():
 
-for tide_number in range(1, 25):
-    j_tide = screen_y - int((j_data[tide_number]["tide"]) * 66 + 73)
-    y = j_tide
-    j_time = (j_data[tide_number]["hour"])
-    print(x, y)
-    x = x + 49
-    point_list.append((x, y))
-    if draw_line == True:
-        time_font = font.render("{}".format(j_time), True, WHITE)
-        time_rect = (x, 720, 30, 20)
-        pygame.draw.line(windowSurface, WHITE, (x, 250), (x, 700))
-        windowSurface.blit(time_font, time_rect)
-        draw_line = False
+point_list, time_labels, time_axis = get_graph(j_data)
 
-    else:
-        draw_line = True
-
-# draw_window = draw_window()
 
 while True:
     for event in pygame.event.get():
@@ -112,46 +141,33 @@ while True:
 
                 redraw = True
                 change_day = change_day + 1
-                date, j_data = delta(change_day)
+                date, j_data = get_tide(change_day)
 
             if reset_rect.collidepoint(mouse_pos):
                 windowSurface.fill((0, 0, 0))
                 redraw = True
                 change_day = 0
-                date, j_data = delta(change_day)
+                date, j_data = get_tide(change_day)
 
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
 
-
+#### If the day + 1 button is clicked, redraw screen
     if redraw == True:
-        x = 10
-        point_list = []
-        for tide_number in range(1, 25):
-            j_tide = screen_y - int((j_data[tide_number]["tide"]) * 66 + 73)
-            y = j_tide
-            j_time = (j_data[tide_number]["hour"])
-            print(x, y)
-            x = x + 49
-            point_list.append((x, y))
-            if draw_line == True:
-                time_font = font.render("{}".format(j_time), True, WHITE)
-                time_rect = (x, 720, 30, 20)
-
-                pygame.draw.line(windowSurface, WHITE, (x, 250), (x, 700))
-                windowSurface.blit(time_font, time_rect)
-                date_font = header_font.render("The date is {}".format(date), True, WHITE)
-                draw_line = False
-
-            else:
-                draw_line = True
+        point_list, time_labels, time_axis = get_graph(j_data)
         redraw = False
 
     for y_tide in range(300, 699, 66):
         pygame.draw.line(windowSurface, WHITE, (10, y_tide), (1190, y_tide))
     pygame.draw.lines(windowSurface, WHITE, False, (point_list))
+
+    index = 0
+    for label in time_labels:
+        windowSurface.blit(label, time_axis[index])
+        index = index + 1
+
     windowSurface.blit(one_tide, one_rect)
     windowSurface.blit(second_tide, second_rect)
     windowSurface.blit(third_tide, third_rect)
@@ -159,6 +175,7 @@ while True:
     windowSurface.blit(fifth_tide, fifth_rect)
     windowSurface.blit(sixth_tide, sixth_rect)
 
+    date_font = header_font.render("The date is {}".format(date), True, WHITE)
     windowSurface.blit(date_font, date_rect)
     windowSurface.blit(day_text, day_rect)
     windowSurface.blit(reset_text, reset_rect)
